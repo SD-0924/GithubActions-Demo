1f5e37c6ab1d8c814e3b6c0d009300f7feaa5e67
AlaaYa7ia <alaa.yahia.1995@gmail.com>
2024-10-22 15:10:14 +0300

    init

---
+++ index.js
@@ -0,0 +1,16 @@
+const http = require("http");
+
+const host = "localhost";
+const port = 8000;
+
+const requestListener = function (req, res) {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World!");
+};
+
+const server = http.createServer(requestListener);
+
+server.listen(port, host, () => {
+  console.log(`Server is running on http://${host}:${port}`);
+});
